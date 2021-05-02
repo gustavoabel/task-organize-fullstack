e8f741d62a2490c1fb4ca81e4aac9fef5421e7af
gustavoabell <gustavoabel.contato@gmail.com>
2021-05-01 21:41:00 -0300

    connect database

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 class App {
 public app: express.Application;
@@ -7,7 +8,23 @@ public app: express.Application;
 public constructor() {
   this.app = express();
   this.app.use(cors());
+  
+  this.initMongoose();
+  this.connectDatabase();
 }
+
+private initMongoose(): void {
+  mongoose.set('runValidators', true);
+}
+private connectDatabase(): void {
+  mongoose.connect('mongodb+srv://gustavoabell:qjyzu8wNPXYMfyjC@cluster0.wcjgz.mongodb.net/fullstack-project?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+}
+
 
 public listen(port: number): void {
   this.app.listen(port, () => {
