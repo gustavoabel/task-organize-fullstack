89a9afb01a624bd567fad5627df542c855cc337f
gustavoabell <gustavoabel.contato@gmail.com>
2021-05-07 00:38:09 -0300

    refactoring the code

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,38 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
 class App {
-public app: express.Application;
+  public app: express.Application;
 
-public constructor() {
-  this.app = express();
-  this.app.use(cors());
-  
-  this.initMongoose();
-  this.connectDatabase();
-}
+  public constructor() {
+    this.app = express();
+    this.app.use(cors());
 
-private initMongoose(): void {
-  mongoose.set('runValidators', true);
-}
-private connectDatabase(): void {
-  mongoose.connect('mongodb+srv://gustavoabell:qjyzu8wNPXYMfyjC@cluster0.wcjgz.mongodb.net/fullstack-project?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  });
-}
+    this.initMongoose();
+    this.connectDatabase();
+  }
 
+  private initMongoose(): void {
+    mongoose.set("runValidators", true);
+  }
+  private connectDatabase(): void {
+    mongoose.connect(
+      "mongodb+srv://gustavoabell:qjyzu8wNPXYMfyjC@cluster0.wcjgz.mongodb.net/fullstack-project?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      }
+    );
+  }
 
-public listen(port: number): void {
-  this.app.listen(port, () => {
-    console.log(`Aplicação iniciada na porta: ${port}`);
-  });
-}
+  public listen(port: number): void {
+    this.app.listen(port, () => {
+      console.log(`Aplicação iniciada na porta: ${port}`);
+    });
+  }
 }
 
 export default App;
