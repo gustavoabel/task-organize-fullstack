1ee674cc17f6e9b8cdb40233ff7581073471144a
Abel, Gustavo <gustavo.silva@vetta.digital>
2021-09-17 23:30:49 -0300

    Revert "Revert "Revert "Revert "fix order paste""""
    This reverts commit 1dc019863d22ada2781015614ea666094b0cc0d5.

--- src/middlewares/NotFoundErrorMiddleware.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-import responseNotFound from '../responses/ResponseNotFound';
-function notFoundErrorMiddleware(req: Request, res: Response) {
-    return responseNotFound(res);
-}
-
-export default notFoundErrorMiddleware;
