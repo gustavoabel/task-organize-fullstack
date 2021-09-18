295ab9b71f578a6ae0612ff92a8258164344718f
Abel, Gustavo <gustavo.silva@vetta.digital>
2021-09-17 23:32:42 -0300

    Revert "Revert "Revert "Revert "Revert "fix order paste"""""
    This reverts commit 1ee674cc17f6e9b8cdb40233ff7581073471144a.

---
+++ src/errors/HttpException.ts
@@ -0,0 +1,15 @@
+import NoContentException from "./NoContentException";
+
+class HttpException extends Error {
+  public status: number;
+  public message: string;
+
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+    this.message = message;
+  }
+}
+
+
+export default HttpException;
